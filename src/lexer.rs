ad6943571ea4763ed5359769e4810e7c060f8634
Prashant <prashantrahul141@protonmail.com>
2024-02-07 01:28:01 +0530

    add top level lexer and constructor for it

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,3 +9,32 @@ pub enum Token {
     Eof,
 }
 
+// top level lexer.
+pub struct Lexer {
+    // source string.
+    pub source_string: String,
+    // source string as vec of chars.
+    pub source_chars: Vec<char>,
+    // start of current token.
+    pub start: usize,
+    // current position of the character in consideration.
+    pub current: usize,
+    // Vec of tokens.
+    pub tokens: Vec<Token>,
+    // len of the input string.
+    pub len: usize,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Self {
+        Self {
+            source_chars: input.chars().collect(),
+            source_string: input.clone(),
+            start: 0,
+            current: 0,
+            len: input.len(),
+            tokens: vec![],
+        }
+    }
+
+}
