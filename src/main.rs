1b4a59e6e7c9768312333035b6a3275ec36af705
Prashant <prashantrahul141@protonmail.com>
2024-02-07 01:30:48 +0530

    adding lexer to main and parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use lexer::Lexer;
+pub mod lexer;
+
 fn main() {
     let input_string = std::env::args().collect::<Vec<String>>()[1..]
         .concat()
         .to_string();
 
     println!("Input String : {}", &input_string);
+
+    let mut lexer = Lexer::new(input_string);
+    lexer.parse();
+
+    for token in lexer.tokens {
+        println!("{:?}", token);
+    }
 }
