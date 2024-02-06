9da844bd625e33b3bf26f73306a76f81e5ebd487
Prashant <prashantrahul141@protonmail.com>
2024-02-07 01:45:54 +0530

    clean up, doc strings.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use lexer::Lexer;
 pub mod lexer;
 
 fn main() {
+    // take input string as command line argument.
     let input_string = std::env::args().collect::<Vec<String>>()[1..]
         .concat()
         .to_string();
 
     println!("Input String : {}", &input_string);
 
+    // start lexer.
     let mut lexer = Lexer::new(input_string);
-    lexer.parse();
+    lexer.scan();
 
     for token in lexer.tokens {
         println!("{:?}", token);
