7c5c130fe24d20f98b63b550f65ccce4876843c1
Prashant <prashantrahul141@protonmail.com>
2024-02-07 01:29:43 +0530

    add helper functions: advance, look_ahead, look_ahead_twice, next, peek, is_at_end, add_token

--- src/lexer.rs
+++ src/lexer.rs
@@ -37,4 +37,45 @@ impl Lexer {
         }
     }
 
+    // consumes next character and returns it.
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source_chars[self.current - 1]
+    }
+
+    // returns next char but doesnt consume it.
+    fn look_ahead(&mut self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+        self.source_chars[self.current]
+    }
+
+    // returns next + 1 but doesnt consume it.
+    fn look_ahead_twice(&mut self) -> char {
+        if self.current + 1 >= self.len {
+            return '\0';
+        }
+        self.source_chars[self.current + 1]
+    }
+
+    // returns the next token, and also consumes it.
+    fn next(&mut self) -> Token {
+        self.tokens.pop().unwrap_or(Token::Eof)
+    }
+
+    // returns the next token, but doesnt consume it.
+    fn peek(&self) -> Token {
+        self.tokens.last().copied().unwrap_or(Token::Eof)
+    }
+
+    // checks if reached the end of the input string.
+    fn is_at_end(&self) -> bool {
+        self.current >= self.len
+    }
+
+    // helper function to add tokens.
+    fn add_token(&mut self, token_type: Token) {
+        self.tokens.push(token_type);
+    }
 }
