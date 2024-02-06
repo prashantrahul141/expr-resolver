484306349a1b785bb4fd8bd587405f035ace664f
Prashant <prashantrahul141@protonmail.com>
2024-02-07 01:45:46 +0530

    clean up, doc strings.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,12 @@
 // List of all the tokens possible.
 #[derive(Debug, Clone, Copy)]
 pub enum Token {
+    // we store numbers as rust's f64.
     Number(f64),
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
+    Plus,
+    Minus,
+    Star,
+    Slash,
     Eof,
 }
 
@@ -26,6 +27,13 @@ pub struct Lexer {
 }
 
 impl Lexer {
+    /// Constructor for lexer
+    ///
+    /// # Arguments
+    /// * input - Input string to tokenize.
+    ///
+    /// # Returns
+    /// * Lexer - new instance of lexer with prepopulated fields.
     pub fn new(input: String) -> Self {
         Self {
             source_chars: input.chars().collect(),
@@ -37,28 +45,34 @@ impl Lexer {
         }
     }
 
-    // public function which starts parsing.
-    pub fn parse(&mut self) {
+    /// Top level public function to start tokenizing the input string.
+    pub fn scan(&mut self) {
         while !self.is_at_end() {
-            self.parse_token();
+            self.scan_token();
             self.start = self.current;
         }
 
+        // we add a EOF token at the end, comes handy when parsing.
         self.add_token(Token::Eof);
+
+        // we reverse the entire tokens vec, because we will be
+        // using pop function to retrieve one token at time, which
+        // takes tokens from the end.
+        self.tokens.reverse();
     }
 
-    // parses one token at at time.
-    fn parse_token(&mut self) {
+    /// Internal function which parses one token at a time.
+    fn scan_token(&mut self) {
         let current_char = self.advance();
 
         match current_char {
-            '+' => self.add_token(Token::Add),
-            '-' => self.add_token(Token::Subtract),
-            '*' => self.add_token(Token::Multiply),
-            '/' => self.add_token(Token::Divide),
+            '+' => self.add_token(Token::Plus),
+            '-' => self.add_token(Token::Minus),
+            '*' => self.add_token(Token::Star),
+            '/' => self.add_token(Token::Slash),
             _ => {
                 if current_char.is_ascii_digit() {
-                    self.parser_number();
+                    self.scan_number();
                 } else {
                     panic!("unsupported character in input.");
                 }
@@ -66,12 +80,13 @@ impl Lexer {
         }
     }
 
-    // parses a number.
-    fn parser_number(&mut self) {
+    /// Scans a number type of token.
+    fn scan_number(&mut self) {
         while self.look_ahead().is_ascii_digit() {
             self.advance();
         }
 
+        // for floating point numbers.
         if self.look_ahead() == '.' && self.look_ahead_twice().is_ascii_digit() {
             self.advance();
             while self.look_ahead().is_ascii_digit() {
@@ -79,23 +94,25 @@ impl Lexer {
             }
         }
 
+        // we take literal string of the number and parse it into rust's f64.
         let number_literal = self.source_string[self.start..self.current]
             .to_string()
             .parse::<f64>();
 
         match number_literal {
             Ok(number_literal) => self.add_token(Token::Number(number_literal)),
+            // probably never going to happen but still you never know.
             Err(_) => panic!("Failed to parse number literal as f64"),
         }
     }
 
-    // consumes next character and returns it.
+    /// consumes current character and returns it.
     fn advance(&mut self) -> char {
         self.current += 1;
         self.source_chars[self.current - 1]
     }
 
-    // returns next char but doesnt consume it.
+    /// returns current character but doesn't consume it.
     fn look_ahead(&mut self) -> char {
         if self.is_at_end() {
             return '\0';
@@ -103,7 +120,7 @@ impl Lexer {
         self.source_chars[self.current]
     }
 
-    // returns next + 1 but doesnt consume it.
+    /// returns next character but doesn't consume it.
     fn look_ahead_twice(&mut self) -> char {
         if self.current + 1 >= self.len {
             return '\0';
@@ -111,12 +128,12 @@ impl Lexer {
         self.source_chars[self.current + 1]
     }
 
-    // returns the next token, and also consumes it.
+    /// returns the next token, and also consumes it.
     fn next(&mut self) -> Token {
         self.tokens.pop().unwrap_or(Token::Eof)
     }
 
-    // returns the next token, but doesnt consume it.
+    /// returns the next token, but doesn't consume it.
     fn peek(&self) -> Token {
         self.tokens.last().copied().unwrap_or(Token::Eof)
     }
