0c984f9f667b723fc1fa97b42a8a308e693d36a8
Prashant <prashantrahul141@protonmail.com>
2024-02-07 23:27:02 +0530

    add ast struct, and display for ast
    clean up

---
+++ src/parser.rs
@@ -0,0 +1,26 @@
+/// Enum for modeling AST nodes.
+pub enum AST {
+    // each node.
+    Node(Token),
+    // connections.
+    Con(Token, Vec<AST>),
+}
+
+/// fmt display for ast
+/// recursively prints AST.
+impl core::fmt::Display for AST {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            AST::Node(token) => write!(f, " {}", token),
+            AST::Con(head, rest) => {
+                write!(f, "({}", head)?;
+                for node in rest {
+                    write!(f, "{}", node)?;
+                }
+
+                write!(f, ")")
+            }
+        }
+    }
+}
+
