61337a1009c5c3d3a95130840cf6801035232199
Prashant <prashantrahul141@protonmail.com>
2024-02-07 23:29:00 +0530

    add expr method and public parse method.

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,6 @@
+use crate::lexer::Lexer;
+use crate::lexer::Token;
+
 /// Enum for modeling AST nodes.
 pub enum AST {
     // each node.
@@ -33,6 +36,44 @@ pub struct Parser<'a> {
 impl<'a> Parser<'a> {
     pub fn new(lexer: &'a mut Lexer) -> Self {
         Self { lexer }
+    }
+
+    pub fn parse(&mut self) -> AST {
+        self.expr(0)
+    }
+
+    fn expr(&mut self, min_binding_power: u8) -> AST {
+        let mut left_hand_side = match self.lexer.next_token() {
+            Token::Number(f) => AST::Node(Token::Number(f)),
+            Token::Eof => panic!("Unexpected token : EOF"),
+            operator => {
+                let ((), right_binding_power) = Parser::prefix_binding_power(operator);
+                let right_hand_side = self.expr(right_binding_power);
+                AST::Con(operator, vec![right_hand_side])
+            }
+        };
+
+        loop {
+            let operator = match self.lexer.peek() {
+                Token::Number(n) => panic!("Expected operator recieved number : {n}"),
+                Token::Eof => break,
+                op => op,
+            };
+
+            let (left_bp, right_bp) = Parser::infix_binding_power(operator);
+
+            if left_bp < min_binding_power {
+                break;
+            }
+
+            self.lexer.next_token();
+
+            let right_hand_side = self.expr(right_bp);
+
+            left_hand_side = AST::Con(operator, vec![left_hand_side, right_hand_side]);
+        }
+
+        left_hand_side
     }
 
     fn infix_binding_power(token: Token) -> (u8, u8) {
