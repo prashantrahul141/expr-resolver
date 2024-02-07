44726da48253631e09195959325192acc1b81dce
Prashant <prashantrahul141@protonmail.com>
2024-02-08 01:33:20 +0530

    add lib.rs to use lexer and parser.

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,33 @@ pub mod interpreter;
 pub mod lexer;
 pub mod parser;
 
-#[cfg(test)]
-mod tests {}
+use lexer::Lexer;
+use parser::Parser;
+
+/// Takes mathematical expression as string, resolves it.
+/// # Arguments
+/// * input_string
+/// # Returns
+/// Result<f_64, String>
+/// # Examples
+/// ```
+/// use expr_resolver::resolve;
+/// assert!(matches!(resolve("2)2".to_string()), Err(String)));
+/// ```
+pub fn resolve(input_string: String) -> Result<(), String> {
+    // create a new lexer
+    let mut lexer = Lexer::new(input_string);
+    // and parse input string into tokens.
+    lexer.scan();
+
+    if lexer.has_errors {
+        return Err("Found lexical error(s) in the expression.".to_string());
+    }
+
+    // create a new parser
+    let mut parser = Parser::new(&mut lexer);
+    // and parse tokens into AST.
+    let _ = parser.parse();
+
+    Ok(())
+}
