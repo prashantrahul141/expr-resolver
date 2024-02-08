64e3acfa8b0861fa25fb46684193f68792a6b245
Prashant <prashantrahul141@protonmail.com>
2024-02-09 01:35:14 +0530

    add doc strings to interpreter.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -36,26 +36,44 @@ impl Interpreter {
         }
     }
 
-    /// calls functions required by recognising the type of expression
+    /// This is nothing but a wrapper which calls functions required
+    /// by recognising the type of expression from the length of operands.
+    /// # Arguments
+    /// * token - Reference to the token.
+    /// * sub_tokens - Reference to vector of ast inside the current node.
+    /// # Returns
+    /// The Result returned by the respective called function.
     fn solve_expr(&self, operator: &Token, sub_tokens: &Vec<AST>) -> Result<f64, String> {
         match sub_tokens.len() {
+            // if there are two operands, the expression is binary.
             2 => return self.solve_binary(operator, sub_tokens),
+            // if there is only one operand, the expression is unary.
             1 => return self.solve_unary(operator, sub_tokens),
+            // everything else is unreal according to this interpreter.
             _ => return Err("Unrecognised number of operands.".to_string()),
         }
     }
 
+    /// Solves a binary expression.
+    /// # Arguments
+    /// * token - Reference to the token.
+    /// * sub_tokens - Reference to vector of ast inside the current node.
+    /// # Returns
+    /// Result with value after solving the binary expresion, otherwise error string.
     fn solve_binary(&self, operator: &Token, sub_tokens: &Vec<AST>) -> Result<f64, String> {
+        // the left operand.
         let left = match self.walk_ast(&sub_tokens[0]) {
             Ok(left) => left,
             Err(e) => return Err(e),
         };
 
+        // the right operand..
         let right = match self.walk_ast(&sub_tokens[1]) {
             Ok(right) => right,
             Err(e) => return Err(e),
         };
 
+        // checking type of operator, and solving accordingly.
         match operator {
             Token::Plus => Ok(left + right),
             Token::Minus => Ok(left - right),
@@ -66,12 +84,20 @@ impl Interpreter {
         }
     }
 
+    /// Solves a binary expression.
+    /// # Arguments
+    /// * token - Reference to the token.
+    /// * sub_tokens - Reference to vector of ast inside the current node.
+    /// # Returns
+    /// Result with value after solving the binary expresion, otherwise error string.
     fn solve_unary(&self, token: &Token, sub_tokens: &Vec<AST>) -> Result<f64, String> {
+        // the only right operand.
         let right = match self.walk_ast(&sub_tokens[0]) {
             Ok(right) => right,
             Err(e) => return Err(e),
         };
 
+        // checking type of operator and solving accordingly.
         match token {
             Token::Minus => return Ok(-right),
             _ => return Err("Unrecognised binary operator.".to_string()),
